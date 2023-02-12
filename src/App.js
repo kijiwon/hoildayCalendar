f304afae5ba9f5df4a0254121238eb2fcddf642e
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-02-12 20:03:24 +0900

    api연결 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { BsFillArrowRightSquareFill } from 'react-icons/bs'
+import HolidayList from './component/HolidayList';
+import axios from 'axios';
 function App() {
+  // 현재 기온 api
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=1329c99004752da584b7ba8078fe61cc`
+  // 년도 선택
+  const [year, setYear]= useState('');
+  const [month, setMonth] = useState('');
+  const url = `https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?solYear=${year}&solMonth=${month}&ServiceKey=871FzwAfTTqVJewFZSr8rPUe2p%2F%2BjEGAaGwjC%2FNDGeHLhfE0%2FuCBIw8vbsiYOBndXZzXg484yvlTiUgg3lprAA%3D%3D`
+  const getHoliday = async()=>{
+    try{
+      const data = await axios({
+      method:'get',
+      url : url
+    })
+    console.log(data)
+    } catch(err){
+      console.log('error')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>우리는 원한다.</h1>
+        {/* <p>현재 기온 : <span>6.0</span>도</p> */}
+        <div>
+          <input 
+            type='text'
+            placeholder='연도'
+            value={year}
+            onChange={(e)=>setYear(e.target.value)}/>
+          <span>년</span>
+          <input
+            type='text'
+            placeholder='월'
+            value={month}
+            onChange={(e)=>setMonth(e.target.value)}
+          />
+          <span>월</span>
+        </div>
+        <h2>의 공휴일</h2>
+        <BsFillArrowRightSquareFill onClick={getHoliday}/>
       </header>
+      <HolidayList/>
     </div>
   );
 }
---
+++ src/component/HolidayList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HolidayList(){
+    return (
+        <div></div>
+    )
+}
+
+
+export default HolidayList;
